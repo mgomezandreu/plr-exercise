2afe4d4fd794214dc186b4fbbdae12d46fc70787
mgomezandreu <mario-gomez-andreu@outlook.com>
2024-03-05 12:16:47 +0100

    added docstrings to all files

--- plr_exercise/models/cnn.py
+++ plr_exercise/models/cnn.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
-
+        """Simple Convolutional Neural Network"""
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
@@ -16,6 +16,7 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
+        """Pass x forward through the network and return the output"""
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
